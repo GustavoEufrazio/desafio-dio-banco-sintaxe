23dd052b870ae03328c0709859a8589a52090434
Gustavo Eufrazio <gustavo.eufrazio0707@gmail.com>
2024-05-24 08:16:24 -0300

    Enviando o projeto

---
+++ src/main/java/com/mycompany/projeto/dio/contabancaria/ContaTerminal.java
@@ -0,0 +1,40 @@
+package com.mycompany.projeto.dio.contabancaria;
+
+import java.util.Scanner;
+
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("///////////////////////////////////////////\n"
+                + "Bem vindo ao Banco!\n"
+                + "///////////////////////////////////////////");
+        //criar uma validação que precissa de uma resposta do while
+        
+        
+        System.out.println("Informe o número da conta: ");
+        numero = scanner.nextInt();
+        //scanner.nextLine();
+        System.out.println("Informe o número da agência: ");
+        agencia = scanner.nextLine();
+        
+        System.out.println("Informe o nome: ");
+        nomeCliente = scanner.nextLine();
+        
+        
+        System.out.println("Informe o saldo: ");
+        saldo = scanner.nextDouble();
+        
+        System.out.println("Olá "+ nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo+" já está disponível para saque");
+        
+        
+           
+         
+    }
+}
